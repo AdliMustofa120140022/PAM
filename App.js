32f46cb4c31acc3738ab1f6b6a41c7f5951e1510
Adli Mustofa <103428648+AdliMustofa120140022@users.noreply.github.com>
2023-03-17 23:30:36 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import Routers from "./src";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const App = () => {
-  
+import HomeScreen from './screen/Home';
+import DetailScreen from './screen/Detail';
+
+const stack = createNativeStackNavigator();
+
+export default function App() {
   return (
     <NavigationContainer>
-      <Routers/>
+      <stack.Navigator initialRouteName="Home">
+        <stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <stack.Screen name="Detail" component={DetailScreen} options={{ headerShown: false }} />
+      </stack.Navigator>
     </NavigationContainer>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
